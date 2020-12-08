8fa5668f5a4cccbf17c6b98ac74affa05e283c43
Felix <felix.meyer97@web.de>
2020-12-08 17:16:20 +0100

    added some init values

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy
+import optimization
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+# Init values of task
+# Weight of the driver; one driver per vehicle
+weight_of_driver = [72.4, 85.7]
+
+# Maximal capacity of one transport car
+max_cap = 1100
+
+# Needed hardware
+# todo matrix/ numpy array?
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
