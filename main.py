261af6018747854d5d7068f3a9de4e75b05fabda
Felix <felix.meyer97@web.de>
2020-12-10 14:50:06 +0100

    built some structure

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-import numpy
+import numpy as np
+import scipy
+
 import optimization
 
+"""
+obj func = max(sum of value of hardware)
+
+constraints: 
+
+"""
+"""
+a) watch combination of driver
+
+1. Calc all value/ weight ratio --> array with ratios
+2. Sort array
+3. Put in the transport car the item with highest value, repeat as long as cap is avail.
+4. Repeat for 2nd car
+notebook13[1] = weight
+{notebook13 : {units: 205} , {weight: 2451}, {value: 40}}
+notebook13[value] -> 40
+"""
 
 
 # Init values of task
@@ -12,9 +31,8 @@ max_cap = 1100
 
 # Needed hardware
 # todo matrix/ numpy array?
-
+hardware = np.array(["Notebook Büro 13", 205, 2451, 40], ["Notebook Büro 14", 420, 2978, 35],
+                    ["Notebook outdoor", 450, 3625, 80], ["Mobiltelefon Büro", 60, 717, 30], [], [], [], [], [], [])
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
-
