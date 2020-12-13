372800219b4be98fa8d46d1dbe27cc711922d40f
Felix <felix.meyer97@web.de>
2020-12-13 12:17:31 +0100

    added push_items function and basic structure for this

--- optimization.py
+++ optimization.py
@@ -8,11 +8,12 @@ def sorted_relative_value(weights, values):
     :param values: values dict
     :return: sorted dict with relatives values
     """
-    relative_values = {} # value/weight
-    print(values["Notebook Büro 14"])
-    print(weights["Notebook Büro 14"])
 
-    print(values["Notebook Büro 14"]/weights["Notebook Büro 14"])
+    relative_values = {} # value/weight
+    # print(values["Notebook Büro 14"])
+    # print(weights["Notebook Büro 14"])
+    #
+    # print(values["Notebook Büro 14"]/weights["Notebook Büro 14"])
 
     # Get each rel. value
     for k in weights:
@@ -22,3 +23,23 @@ def sorted_relative_value(weights, values):
     relative_values = {k: v for k, v in sorted(relative_values.items(), key=lambda item: item[1], reverse=True)}
 
     return relative_values
+
+def push_items_on_car(load_cap, driver_weight, rel_values):
+    """
+    Puts the items with the highest value on the car, until no items are necc. anymore. Then take the item with the
+    second highest rel. value. Repeat this with further items until the car is at full capacity
+    :param load_cap: capacity of car
+    :param driver_weight: array with weights of driver
+    :param rel_values: sorted dict with relative values (values/ weight)
+    :return: items on car todo as dict?
+    """
+    # Track current capacity
+    curr_cap = 0
+
+    # Get most valued item and push on load (todo list.append?)
+    # In while loop to track capacity?
+    while curr_cap <= load_cap:
+        pass
+
+
+
